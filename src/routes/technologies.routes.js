b61288e22bd5f4a8f68698ff7f17cb38845e6a27
Extibax <extibax@gmail.com>
2019-05-05 03:00:12 -0500

    try-catch added

--- src/routes/technologies.routes.js
+++ src/routes/technologies.routes.js
@@ -7,95 +7,115 @@ const router = Router();
 
 /* Get All Technologies*/
 router.get("/", async (req, res) => {
-  const database = await connect();
-  const result = await database
-    .collection("technologies")
-    .find({})
-    .toArray();
-  console.log(result);
-  res.json(result);
+  try {
+    const database = await connect();
+    const result = await database
+      .collection("technologies")
+      .find()
+      .toArray();
+    console.log(result);
+    res.json(result);
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 /* Get One Technologie */
 router.get("/:id", async (req, res) => {
-  const { id } = req.params;
-  const database = await connect();
-  const result = await database.collection("technologies").findOne({
-    _id: ObjectID(id)
-  });
-  console.log(result);
-  res.json(result);
+  try {
+    const { id } = req.params;
+    const database = await connect();
+    const result = await database.collection("technologies").findOne({
+      _id: ObjectID(id)
+    });
+    console.log(result);
+    res.json(result);
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 /* Post Technologie */
 router.post("/", async (req, res) => {
-  const database = await connect();
-  const technologie = {
-    name: req.body.name,
-    like: req.body.like,
-    info: {
-      experience: req.body.info.experience,
-      description: req.body.info.description
-    }
-  };
-  const result = await database
-    .collection("technologies")
-    .insertOne(technologie);
-  console.log({
-    message: `Technologie ${req.body.name} created`,
-    result: result.ops[0]
-  });
-  res.json({
-    message: `Technologie ${req.body.name} created`,
-    result: result.ops[0]
-  });
+  try {
+    const database = await connect();
+    const technologie = {
+      name: req.body.name,
+      like: req.body.like,
+      info: {
+        experience: req.body.info.experience,
+        description: req.body.info.description
+      }
+    };
+    const result = await database
+      .collection("technologies")
+      .insertOne(technologie);
+    console.log({
+      message: `Technologie ${req.body.name} created`,
+      result: result.ops[0]
+    });
+    res.json({
+      message: `Technologie ${req.body.name} created`,
+      result: result.ops[0]
+    });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 /* Put (Update) Technologie */
 router.put("/:id", async (req, res) => {
-  const { id } = req.params;
-  const technologie_update = {
-    name: req.body.name,
-    like: req.body.like,
-    info: {
-      experience: req.body.info.experience,
-      description: req.body.info.description
-    }
-  };
-  const database = await connect();
-  const result = await database.collection("technologies").updateOne(
-    {
-      _id: ObjectID(id)
-    },
-    {
-      $set: technologie_update
-    }
-  );
-  console.log({
-    message: `Technologie with id: ${id} updated`,
-    result
-  });
-  res.json({
-    message: `Technologie with id: ${id} updated`,
-    result
-  });
+  try {
+    const { id } = req.params;
+    const technologie_update = {
+      name: req.body.name,
+      like: req.body.like,
+      info: {
+        experience: req.body.info.experience,
+        description: req.body.info.description
+      }
+    };
+    const database = await connect();
+    const result = await database.collection("technologies").updateOne(
+      {
+        _id: ObjectID(id)
+      },
+      {
+        $set: technologie_update
+      }
+    );
+    console.log({
+      message: `Technologie with id: ${id} updated`,
+      result
+    });
+    res.json({
+      message: `Technologie with id: ${id} updated`,
+      result
+    });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 /* Delete Technologie */
 router.delete("/:id", async (req, res) => {
-  const { id } = req.params;
-  const database = await connect();
-  const result = await database.collection("technologies").deleteOne({
-    _id: ObjectID(id)
-  });
-  console.log({
-    message: `Technologie with id: ${id} deleted`,
-    result
-  });
-  res.json({
-    message: `Technologie with id: ${id} deleted`,
-    result
-  });
+  try {
+    const { id } = req.params;
+    const database = await connect();
+    const result = await database.collection("technologies").deleteOne({
+      _id: ObjectID(id)
+    });
+    console.log({
+      message: `Technologie with id: ${id} deleted`,
+      result
+    });
+    res.json({
+      message: `Technologie with id: ${id} deleted`,
+      result
+    });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 export default router;
